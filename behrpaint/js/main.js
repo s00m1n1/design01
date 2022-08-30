71dc442e3474ecd48e8696cb2007d5ec9d805a95
kim su min <sumink4818@gmail.com>
2022-08-30 17:56:50 +0900

    js수정

--- behrpaint/js/main.js
+++ behrpaint/js/main.js
@@ -21,17 +21,34 @@ $(document).ready(function(){
     // 조건 2 - 스크롤 값이 0이면 header에 fixed class 삭제
     /*로딩했을 때 맨처음*/ 
     let scrolling = $(window).scrollTop() 
-    console.log(scrolling)
+    headerFixded()//함수실행
+    
 
     /*스크롤 할 때마다 실행*/ 
     $(window).scroll(function(){
+        headerFixded()//함수실행
+    })
+
+
+    function headerFixded(){ //함수선언
         scrolling = $(window).scrollTop()
-        console.log(scrolling)
         if(scrolling > 500){
             $('header').addClass('fixed')    
         }else{
             $('header').removeClass('fixed')    
         }
+    }
+
+    /*header nav에 마우스를 올리면 header에 class추가 */
+
+    $('header nav>ul').on('mouseenter focusin', function(){ /*nav ul에 오버했을때*/ 
+        $('header').addClass('open')
+    })
+    $('header').on('mouseleave', function(){    /*nav block에서 벗어나면 사라져서 백그라운드 컬러부분 까지 다 잡아주기 위해header영역으로 바꿔줘야함*/ 
+        $('header').removeClass('open')
+    })
+    $('header nav>ul>li:last-child>ul>li:last-child').on('focusout', function(){   
+        $('header').removeClass('open')
     })
 })
 
