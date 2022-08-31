8a75bb157a2840287e332c320219732a6f1934f4
kim su min <sumink4818@gmail.com>
2022-08-31 18:01:12 +0900

    upload

--- behrpaint/js/main.js
+++ behrpaint/js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     console.log(winW)
     console.log(docW)
     
-    $('header').addClass('fixed')
-    $('header').removeClass('fixed')
+    // $('header').addClass('fixed')
+    // $('header').removeClass('fixed')
 
     // TOP버튼을 클릭했을때 상단으로 스크롤
     $('aside button').on('click', function(){
@@ -20,7 +20,7 @@ $(document).ready(function(){
     // 조건 1 - 스크롤 값이 0보다 크면 header에 fixed class 추가
     // 조건 2 - 스크롤 값이 0이면 header에 fixed class 삭제
     /*로딩했을 때 맨처음*/ 
-    let scrolling = $(window).scrollTop() 
+    let scrolling 
     headerFixded()//함수실행
     
 
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
     function headerFixded(){ //함수선언
         scrolling = $(window).scrollTop()
-        if(scrolling > 500){
+        if(scrolling > 0){
             $('header').addClass('fixed')    
         }else{
             $('header').removeClass('fixed')    
@@ -41,13 +41,13 @@ $(document).ready(function(){
 
     /*header nav에 마우스를 올리면 header에 class추가 */
 
-    $('header nav>ul').on('mouseenter focusin', function(){ /*nav ul에 오버했을때*/ 
+    $('header nav > ul').on('mouseenter focusin', function(){ /*nav ul에 오버했을때*/ 
         $('header').addClass('open')
     })
     $('header').on('mouseleave', function(){    /*nav block에서 벗어나면 사라져서 백그라운드 컬러부분 까지 다 잡아주기 위해header영역으로 바꿔줘야함*/ 
         $('header').removeClass('open')
     })
-    $('header nav>ul>li:last-child>ul>li:last-child').on('focusout', function(){   
+    $('header nav > ul > li:last-child > ul > li:last-child').on('focusout', function(){   
         $('header').removeClass('open')
     })
 })
